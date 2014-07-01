4f9e0aa1cd78ade45ee2a94e0223bcb7674c5c29
kieranlynam <kieranlynam@gmail.com>
2014-07-01 21:02:45 +0100

    Fixed: Exception in tests: The current SynchronizationContext may not be used as a TaskScheduler

--- Test/ViewModel.Tests/BaseTestContext.cs
+++ Test/ViewModel.Tests/BaseTestContext.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using CoffeeClientPrototype.Model;
 
 namespace ViewModel.Tests
 {
     public class BaseTestContext : IDisposable
     {
+        private readonly SynchronizationContext savedSynchronizationContext;
+
         public List<Cafe> Cafes { get { return this.DataService.Cafes; } }
 
         public MockDataService DataService { get; private set; }
@@ -16,10 +19,13 @@ namespace ViewModel.Tests
         {
             this.DataService = new MockDataService();
             this.IdentityService = new MockIdentityService();
+            this.savedSynchronizationContext = SynchronizationContext.Current;
+            SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
         }
 
         public void Dispose()
         {
+            SynchronizationContext.SetSynchronizationContext(this.savedSynchronizationContext);
         }
     }
 }
