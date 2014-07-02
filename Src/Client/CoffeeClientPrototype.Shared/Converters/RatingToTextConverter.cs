d6b0a55edaecca1b0e51e1f58ad64becfbba9bdc
kieranlynam <kieranlynam@gmail.com>
2014-07-02 21:55:54 +0100

    Include rating on main page

---
+++ Src/Client/CoffeeClientPrototype.Shared/Converters/RatingToTextConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace CoffeeClientPrototype.Converters
+{
+    public sealed class RatingToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var rating = (double) value;
+            return string.Format("{0}/5", rating);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Src/Client/CoffeeClientPrototype.Shared/Converters/VotesCountToTextConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace CoffeeClientPrototype.Converters
+{
+    public sealed class VotesCountToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return string.Format("{0} votes", value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
