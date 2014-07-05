1174796417f2238084eee19eed6400a02c892e94
kieranlynam <kieranlynam@gmail.com>
2014-07-05 12:03:44 +0100

    Include reviews on details page on phone

---
+++ Src/Client/CoffeeClientPrototype.Shared/Converters/CountToStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using Windows.UI.Xaml.Data;
+
+namespace CoffeeClientPrototype.Converters
+{
+    public sealed class StringFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var format = parameter as string;
+
+            if (string.IsNullOrEmpty(format))
+            {
+                throw new NotSupportedException("Must specify a format string (as a converter parameter)");
+            }
+
+            return string.Format(format, value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
