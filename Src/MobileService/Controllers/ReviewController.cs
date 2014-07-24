172b6a862f6b8303724b9743340d1220cf501cfe
kieranlynam <kieranlynam@gmail.com>
2014-07-24 21:19:10 +0100

    Add missing review entity files to mobile services

---
+++ Src/MobileService/Controllers/ReviewController.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.OData;
+using Microsoft.WindowsAzure.Mobile.Service;
+using londoncoffeeService.DataObjects;
+using londoncoffeeService.Models;
+
+namespace londoncoffeeService.Controllers
+{
+    public class ReviewController : TableController<ReviewData>
+    {
+        protected override void Initialize(HttpControllerContext controllerContext)
+        {
+            base.Initialize(controllerContext);
+            var context = new DataContext();
+            this.DomainManager = new EntityDomainManager<ReviewData>(context, this.Request, this.Services);
+        }
+
+        public IQueryable<ReviewData> GetAllReviewData()
+        {
+            return this.Query(); 
+        }
+
+        public SingleResult<ReviewData> GetReviewData(string id)
+        {
+            return this.Lookup(id);
+        }
+
+        public Task<ReviewData> PatchReviewData(string id, Delta<ReviewData> patch)
+        {
+            return this.UpdateAsync(id, patch);
+        }
+
+        public async Task<IHttpActionResult> PostReviewData(ReviewData item)
+        {
+            ReviewData current = await InsertAsync(item);
+            return this.CreatedAtRoute("Tables", new { id = current.Id }, current);
+        }
+
+        public Task DeleteReviewData(string id)
+        {
+             return this.DeleteAsync(id);
+        }
+    }
+}
---
+++ Src/MobileService/DataObjects/ReviewData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace londoncoffeeService.DataObjects
+{
+    [Table("Reviews")]
+    public class ReviewData : EntityData
+    {
+        public string Comment { get; set; }
+
+        public double? CoffeeRating { get; set; }
+
+        public double? AtmosphereRating { get; set; }
+
+        public string SubmittedBy { get; set; }
+
+        public DateTime SubmittedDate { get; set; }
+    }
+}
