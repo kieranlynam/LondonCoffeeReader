1db96c480ad31faebd211998362ac2c590b4c4c6
kieranlynam <kieranlynam@gmail.com>
2014-08-10 12:17:23 +0100

    Caching credentials

--- Src/Client/CoffeeClientPrototype.Shared/Services/AzureIdentityService.cs
+++ Src/Client/CoffeeClientPrototype.Shared/Services/AzureIdentityService.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Windows.Security.Credentials;
+using CoffeeClientPrototype.Model;
 using CoffeeClientPrototype.ViewModel.Services;
 using Microsoft.WindowsAzure.MobileServices;
 
@@ -10,33 +11,109 @@ namespace CoffeeClientPrototype.Services
     public class AzureIdentityService : IIdentityService
     {
         private readonly MobileServiceClient serviceClient;
+        private readonly PasswordVault vault;
 
         public string Id { get; private set; }
 
         public bool IsAuthenticated { get; private set; }
 
+        public AzureIdentityService(MobileServiceClient serviceClient)
+        {
+            this.serviceClient = serviceClient;
+            this.vault = new PasswordVault();
+
+            PasswordCredential credential;
+            if (TryGetCachedCredential(out credential))
+            {
+                this.Id = credential.UserName;
+            }
+        }
+
         public async Task<bool> AuthenticateAsync()
         {
-            try
-            {
-                var user = await this.serviceClient
-                                        .LoginAsync(MobileServiceAuthenticationProvider.MicrosoftAccount);
+            this.Id = null;
+            this.IsAuthenticated = false;
 
-                this.Id = user.UserId;
-                this.IsAuthenticated = true;
-            }
-            catch (InvalidOperationException)
+            PasswordCredential credential;
+
+            if (TryGetCachedCredential(out credential))
             {
-                this.Id = null;
-                this.IsAuthenticated = false;
+                var user = new MobileServiceUser(credential.UserName);
+                credential.RetrievePassword();
+                user.MobileServiceAuthenticationToken = credential.Password;
+
+                this.serviceClient.CurrentUser = user;
+
+                if (await HasTokenExpired())
+                {
+                    vault.Remove(credential);
+                }
+                else
+                {
+                    this.Id = user.UserId;
+                    this.IsAuthenticated = true;
+                }
+            }
+
+            if (!this.IsAuthenticated)
+            {
+                try
+                {
+                    var user = await this.serviceClient
+                        .LoginAsync(MobileServiceAuthenticationProvider.MicrosoftAccount);
+
+                    credential = new PasswordCredential(
+                        MobileServiceAuthenticationProvider.MicrosoftAccount.ToString(),
+                        user.UserId,
+                        user.MobileServiceAuthenticationToken);
+                    vault.Add(credential);
+
+                    this.Id = user.UserId;
+                    this.IsAuthenticated = true;
+                }
+                catch (MobileServiceInvalidOperationException)
+                {
+                }
             }
 
             return this.IsAuthenticated;
         }
 
-        public AzureIdentityService(MobileServiceClient serviceClient)
+        private bool TryGetCachedCredential(out PasswordCredential credential)
         {
-            this.serviceClient = serviceClient;
+            try
+            {
+                credential = vault
+                    .FindAllByResource(MobileServiceAuthenticationProvider.MicrosoftAccount.ToString())
+                    .FirstOrDefault();
+                return true;
+            }
+            catch
+            {
+                credential = null;
+                return false;
+            }
+        }
+
+        private async Task<bool> HasTokenExpired()
+        {
+            try
+            {
+                // Try to return an item now to determine if the cached credential has expired.
+                await this.serviceClient
+                    .GetTable<Cafe>()
+                    .Take(1)
+                    .ToListAsync();
+            }
+            catch (MobileServiceInvalidOperationException ex)
+            {
+                if (ex.Response.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
